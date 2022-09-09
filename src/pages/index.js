cbbfe85b3001579ef13395d1473de15d4400e336
egilgamesh <egilgamesh@outlook.com>
2022-09-09 17:31:51 +0200

    cleaning mainpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,16 +20,6 @@ function HomepageHeader() {
         Your browser does not support the video tag.
       </video>
     </div>
-	  {/* <h1 className="hero__title">{siteConfig.title}</h1>*/}
-		  {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
-			  {/* <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-			  </div>*/}
-
     </header>
   );
 }
